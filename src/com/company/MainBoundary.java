c2c1e6c3f952f8abdbc27009be30146d77f6b4fa
CocoaBuns <62471279+CocoaBuns@users.noreply.github.com>
2020-03-27 00:49:18 +0800

    Add files via upload

--- src/com/company/MainBoundary.java
+++ src/com/company/MainBoundary.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.Scanner;
 
-public class MainBoundary implements IBoundary
+public class MainBoundary extends Boundary
 {
 	private Scanner sc;
 
@@ -11,35 +11,19 @@ public class MainBoundary implements IBoundary
 		sc = new Scanner(System.in);
 	}
 
-	@Override
-	public int process()
+	protected void printMenu()
 	{
-		printMenu();
-		int sel=sc.nextInt();
-		return sel;
-	}
-
-	private void printMenu()
-	{
-		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-		System.out.println("~~~~~~~ Welcome to Hotel Reservation App ~~~~~~~");
-		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-		System.out.println("1 - Add/Manage Guests");
-		System.out.println("2 - Add/Manage Reservations");
-		System.out.println("3 - Room Services");
-		printDivider();
-		System.out.println("4 - Room Check-in / Check-out");
-		printDivider();
-		System.out.println("5 - Manage Hotel Rooms");
-		System.out.println("6 - Other Admin");
-		printDivider();
-		System.out.println("0 - Exit Application");
-		printDivider();
+		printMainTitle("Welcome to Hotel Reservation App");
+		String[] menuList=
+				{
+						"Add/Manage Guests",
+						"Add/Manage Reservations",
+						"Room Services",
+						"Room Check-in / Check-out",
+						"Manage Hotel Rooms",
+						"Other Admin"
+				};
+		printMenuList(menuList,"Exit Application");
 		System.out.println();
-	}
-
-	private void printDivider()
-	{
-		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 	}
 }
