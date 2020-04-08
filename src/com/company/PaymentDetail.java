847b44570a671f759f45f3cf91e729fb8e10d88a
Kenny Voo <kennyvoo@gmail.com>
2020-04-08 12:37:08 +0800

    set added

--- src/com/company/PaymentDetail.java
+++ src/com/company/PaymentDetail.java
@@ -1,8 +1,10 @@
+package com.company;
+
 
 public class PaymentDetail {
 	private PaymentType payment_method;
 	private String card_name;
-	private int card_no;
+	private String card_no;
 	private String billing_address;
 	private String expiry_date;
 	
@@ -13,9 +15,9 @@ public class PaymentDetail {
 	public PaymentDetail(String payment_method) {
 		this.payment_method=PaymentType.valueOf(payment_method);
 	}
-	
+
 	//by card
-	public PaymentDetail(String payment_method,String card_name,int card_no, String billing_address,String expiry_date) {
+	public PaymentDetail(String payment_method,String card_name,String card_no, String billing_address,String expiry_date) {
 		this.payment_method=PaymentType.valueOf(payment_method);
 		this.card_name=card_name;
 		this.card_no=card_no;
@@ -26,6 +28,23 @@ public class PaymentDetail {
 	
 	public String getPaymentMethod() {
 		return payment_method.toString();
+	}
+	
+	public void setPaymentMethod(String method) {
+		this.payment_method=PaymentType.valueOf(method);
+	}
+	
+	public void setCardName(String name) {
+		this.card_name=name;
+	}
+	public void setCardNo(String cardNo) {
+		this.card_no=cardNo;
+	}
+	public void setBillingAddress(String address) {
+		this.billing_address=address;
+	}
+	public void setExpiryDate(String date) {
+		this.expiry_date=date;
 	}
 	
 	public String toString() {
