886009852cac467e8ccf203248ffca9e68a694eb
Kenny Voo <kennyvoo@gmail.com>
2020-04-08 12:37:32 +0800

    Added Process Main

--- src/com/company/PaymentController.java
+++ src/com/company/PaymentController.java
@@ -1,38 +1,165 @@
+package com.company;
 
-
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class PaymentController extends Controller{
 	private ArrayList<PaymentBill> PaymentBillList;
 	private double service_charge=10.0;
 	private double GST =7.0;
-   	PaymentBoundary paymentboundary= new PaymentBoundary();
+	PaymentBoundary paymentboundary= new PaymentBoundary();
+	private Scanner sc = new Scanner(System.in);
 	
-
 	public PaymentController() {
 		PaymentBillList = new ArrayList<PaymentBill>();
 	}
 	
+	@Override
+	public void processMain() {
+		int id;
+		boolean loop = true;
+		while (loop)
+		{
+		int sel = paymentboundary.process();
+			loop = false;
+			switch (sel)
+			{
+				case 1:
+					//Add/Edit Payment Account
+					modifyPaymentAccount();
+					break;
+				case 2:
+					//Add to bill
+					addToBill();
+					break;
+				case 3:
+					//Print Invoice
+					id=paymentboundary.requestReservationID();
+					printInvoice(id);
+					break;
+				case 4:
+					//Make Payment
+					id=paymentboundary.requestReservationID();
+					makePayment(id);
+					break;
+				case 5:
+					//Generate Financial report
+					//printAllGuest();
+					break;
+				case 0:
+					return;
+				default:
+					loop = true;
+					paymentboundary.invalidInputWarning();
+			}
+		}
+		paymentboundary.waitInput();
+		
+	} 
+	
+	public void modifyPaymentAccount() {
+		paymentboundary.modifyAccountMenu();
+		boolean loop = true;
+		while (loop)
+		{
+		int sel = sc.nextInt();
+			loop = false;
+			switch (sel)
+			{
+				case 1:
+					//Add Payment Account
+					createPaymentAccount();
+					break;
+				case 2:
+					//modify Payment Account
+					updatePaymentAccount();
+					break;
+				case 0:
+					return;
+				default:
+					loop = true;
+					paymentboundary.invalidInputWarning();
+			}
+		}
+		paymentboundary.waitInput();
+	}
+
 	//Create payment account when guest and reservation is made.
-	public void createPaymentAccount(Reservation reservation, Guest guest) {
-		PaymentBill bill =new PaymentBill( reservation.getRoomID, reservation.getReservationID, guest.getPaymentDetail());
+	public void createPaymentAccount() {
+		paymentboundary.CreatePaymentAccount();
+		PaymentBill bill =new PaymentBill();
+		System.out.println("Room ID :");
+		bill.setRoomID(sc.nextInt());
+		System.out.println("Reservation ID :");
+		bill.setReservationID(sc.nextInt());
+		bill.setPaymentDetail(createPaymentDetail());
 		PaymentBillList.add(bill);
 	}
 	
+	// Enter Card Detail
+	public PaymentDetail createPaymentDetail() {
+		paymentboundary.CreatePaymentDetail();
+		PaymentDetail card = new PaymentDetail("CARD");
+		System.out.println("Card Name :");
+		card.setCardName(sc.next());
+		System.out.println("Card No :");
+		card.setCardNo(sc.next());	
+		System.out.println("Billing Address:");
+		card.setBillingAddress(sc.next());
+		System.out.println("Card Expiry Date :");
+		card.setExpiryDate(sc.next());
+		return card;
+	}
+	
+	// Update Card Detail
+	public void updatePaymentAccount() {
+		
+	}
+	
+	public void addToBill() {
+		int id=paymentboundary.requestReservationID();
+		paymentboundary.addItemMenu();
+		boolean loop = true;
+		while (loop)
+		{
+			int sel = sc.nextInt();
+			loop = false;
+			switch (sel)
+			{
+				case 1:
+					//Add room
+					addRoomToPaymentBill(id);
+					break;
+				case 2:
+					//Add roomService
+					addRoomServiceToPaymentBill(id);
+					break;
+				case 0:
+					return;
+				default:
+					loop = true;
+					paymentboundary.invalidInputWarning();
+			}
+		}
+		paymentboundary.waitInput();
+		
+	}
+	
+	
 	//add the room to PaymentBill.
-    public void addRoomToPaymentBill(Reservation reservation) {
+    public void addRoomToPaymentBill(int reservationID) {
        /*
         1.get the start and end date,roomID from reservation list
         2. getRoomDetails(roomID); do you have smtg like this that return room based on id?
         3. iterate through the date and add to PaymentBill based on diff rate(weekend)  
         */
-    	Transaction newtrans = new Transaction(item.name, item.description, item.price,quantity, item.date);
+    	Transaction newtrans = new Transaction();
     	PaymentBill bill= getPaymentBill(reservationID);
-    	bill.AddTransaction(item);
+    	//bill.AddTransaction();
     }
     
     //add room service to PaymentBill.
-    public void addRoomServiceToPaymentBill(Reservation reservation) {
+    public void addRoomServiceToPaymentBill(int reservationID) {
 
     	//insert code
     }
@@ -49,12 +176,12 @@ public class PaymentController extends Controller{
 
     //print the invoice
     public void printInvoice(int reservationID) {
+
     	PaymentBill bill=getPaymentBill(reservationID);
     	bill.printPaymentBill();
-    	double totalPaymentBill= calculatePaymentBill(PaymentBill);
-	
-    	System.out.println("The total price :" +totalPaymentBill +" ( Include GST :"+ GST*100+"% ,Service Charge:
-		+service charge*100+" %, Discount: "+bill.getDiscount()*100+"% )")
+    	double totalPaymentBill= calculatePaymentBill(bill);
+    	System.out.println("The total price :" +totalPaymentBill +" ( Include GST :"+ GST*100+"% ,Service Charge:"
+		+service_charge*100+" %, Discount: "+bill.getDiscount()*100+"% )");
 
     }
     
@@ -62,18 +189,15 @@ public class PaymentController extends Controller{
     public void makePayment(int reservationID) {
     	//print the invoice
     	printInvoice(reservationID);
-    	
     	//get the payment method
     	PaymentBill bill=getPaymentBill(reservationID);
-    	PaymentDetail payment =PaymentBill.getPaymentDetail();
-	
-    	if(payment.getPaymentMethod()=="CASH") {
+    	if(bill.getPaymentDetail().getPaymentMethod()=="CASH") {
     		
-    		System.out.println("PAY BY CASH:");
-    		System.out.println("you paid "+ calculatePaymentBill());
+    		System.out.println("Pay By Cash:");
+    		System.out.println("Total Amount: "+ calculatePaymentBill(bill));
     	}
     	else {
-			System.out.println(payment.toString());	
+			System.out.println(bill.getPaymentDetail().toString());	
     	}
 		System.out.println("Thank you");
     	bill.setStatus("PAID");
@@ -104,8 +228,12 @@ public class PaymentController extends Controller{
 
 	public void setGST(double gst) {
 		this.GST= gst;	
-	} 
+	}
 
+
+
+	
+	
     /*
      TO-DO
      
