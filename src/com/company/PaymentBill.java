3d9d90bfafbf94e584b01994d45b2cb22848fbff
Kenny Voo <kennyvoo@gmail.com>
2020-04-08 12:36:46 +0800

    set added

--- src/com/company/PaymentBill.java
+++ src/com/company/PaymentBill.java
@@ -1,3 +1,4 @@
+package com.company;
 
 import java.util.ArrayList;
 
@@ -7,13 +8,16 @@ public class PaymentBill {
 	private double discount=0;
 	private Status status=Status.PENDING;
 	private ArrayList<Transaction> transactionList;
-	private PaymentDetail payment_detail;
+	private PaymentDetail payment_detail= new PaymentDetail("CASH"); //default by cash
 
 	
 	public enum Status{
 		PENDING,PAID,CANCELLED;	
 	}
-	
+	public PaymentBill() {
+		transactionList = new ArrayList<Transaction>();
+	}
+/*	
 	//Create a PaymentBill for a customer
 	public PaymentBill(int roomID, int reservationID,PaymentDetail payment_detail) {
 		this.roomID=roomID;
@@ -21,7 +25,7 @@ public class PaymentBill {
 		this.payment_detail= payment_detail;
 		transactionList = new ArrayList<Transaction>();
 	}
-
+*/
 	//Add transaction to the PaymentBill. As of now, we only have room and roomservice
 	public void AddTransaction(Transaction item) {
 		//Transaction newtrans = new Transaction(item.name, item.description, item.price,quantity, item.date);
@@ -50,14 +54,26 @@ public class PaymentBill {
 		return this.status.toString();
 	}
 	
-	public int getroomID() {
+	public void setRoomID(int roomID) {
+		 this.roomID=roomID;
+	}
+	
+	public int getRoomID() {
 		return this.roomID;
+	}
+	
+	public void setReservationID(int reservationID) {
+		 this.reservationID=reservationID;
 	}
 	
 	public int getReservationID() {
 		return this.reservationID;
 	}
 
+	public void setPaymentDetail(PaymentDetail payment) {
+		 this.payment_detail=payment;
+	}
+	
 	//return the paymentdetail
 	public PaymentDetail getPaymentDetail() {
 		
