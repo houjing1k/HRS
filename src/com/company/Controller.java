26e924b940e48ad2e5ceb74479471d9df806fe4d
Damien Cheng <damiencheng@icloud.com>
2020-04-08 15:27:00 +0800

    Updated Controller.java toFile fromFile functions to be more generic

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -20,31 +20,40 @@ public abstract class Controller {
 		return choice;
 	}
 
-	public void toFile(ArrayList<?> arrayList, String fileName)
+	public <T> void toFile(T object, String fileName)
 	{
 		try {
 			FileOutputStream fos = new FileOutputStream(fileName);
 			ObjectOutputStream oos = new ObjectOutputStream(fos);
-			oos.writeObject(arrayList); // write MenuArray to ObjectOutputStream
+
+			oos.writeObject(object); // write object to ObjectOutputStream
+
 			oos.close();
+			fos.close();
+
 		} catch(Exception ex) {
 			ex.printStackTrace();
 		}
 	}
-
-	public ArrayList<?> fromFile(String fileName)
+	
+	@SuppressWarnings("unchecked")
+	public <T> T fromFile(String fileName)
 	{
+		T object = null;
 		try {
 			FileInputStream fis = new FileInputStream(fileName);
 			ObjectInputStream ois = new ObjectInputStream(fis);
-			ArrayList <?> arrayList;
-			arrayList = (ArrayList <?>)ois.readObject();
+
+			object = (T) ois.readObject();
+			
 			ois.close();
-			return arrayList;
+			fis.close();
+
 		} catch(Exception ex) {
+			//commented to prevent exception from printing
 			//ex.printStackTrace();
-			return new ArrayList<>();
 		}
+		return object;
 	}
 
 	public abstract void processMain();
