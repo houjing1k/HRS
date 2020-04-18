e56a11bae8beef98c71231baf1eef202f1668429
wndxwilson <taiwilson5@gmail.com>
2020-04-19 04:55:12 +0800

    added reserve room

--- src/com/company/ReservationController.java
+++ src/com/company/ReservationController.java
@@ -110,6 +110,7 @@ public class ReservationController extends Controller {
                 reservations.add(new ReservationEntity(startDate, endDate, tempRoomID, newReservationId, guestId, ReservationEntity.ReservationState.CONFIRMED));
                 saveReservationsToFile();
                 reservationBoundary.printRoomHasBeenReserved(tempRoomID);
+                RoomController.getInstance().reserve(tempRoomID, guestId, newReservationId);
                 reserved = true;
                 break;
             }
